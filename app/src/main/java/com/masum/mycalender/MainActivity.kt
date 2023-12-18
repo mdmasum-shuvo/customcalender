a53ca3e44e665320e1485e3908c101c6a7f8efd9
masumappinion <masumappinion@gmail.com>
2023-12-18 13:12:18 +0600

    calender q-map done

--- app/src/main/java/com/masum/mycalender/MainActivity.kt
+++ app/src/main/java/com/masum/mycalender/MainActivity.kt
@@ -29,33 +29,30 @@ class MainActivity : ComponentActivity() {
 
 
         setContent {
-            MycalenderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val selectedDate = remember {
-                        mutableStateOf("")
-                    }
-                    eventList.add("01/12/2023")
-                    eventList.add("09/12/2023")
-                    eventList.add("10/12/2023")
-                    eventList.add("11/12/2023")
-                    eventList.add("12/12/2023")
-                    eventList.add("17/12/2023")
-                    eventList.add("30/12/2023")
-                    eventList.add("27/12/2023")
-                    eventList.add("27/11/2023")
-                    viewModel.createEvent(eventList)
-                    viewModel.generateCalender()
-
-                    LaunchedEffect(key1 = selectedDate.value, block = {
-                        //do here
-                        Log.e("selected date", "select date:" + selectedDate.value)
-                    })
-                    MainCalenderScreen(viewModel, selectedDate = selectedDate)
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                val selectedDate = remember {
+                    mutableStateOf("")
                 }
+                eventList.add("01/12/2023")
+                eventList.add("09/12/2023")
+                eventList.add("10/12/2023")
+                eventList.add("11/12/2023")
+                eventList.add("12/12/2023")
+                eventList.add("17/12/2023")
+                eventList.add("30/12/2023")
+                eventList.add("27/12/2023")
+                eventList.add("27/11/2023")
+                viewModel.createEvent(eventList)
+                viewModel.generateCalender()
+
+                LaunchedEffect(key1 = selectedDate.value, block = {
+                    //do here
+                    Log.e("selected date", "select date:" + selectedDate.value)
+                })
+                MainCalenderScreen(viewModel, selectedDate = selectedDate)
             }
         }
     }
